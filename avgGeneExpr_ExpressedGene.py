07efc359739093e6ef60af16101353cf853c887f
Liam McKay <ltmckay@ucsc.edu>
2018-03-04 23:29:30 -0800

    boxplot and scattered colored corrected alpha bump

--- avgGeneExpr_ExpressedGene.py
+++ avgGeneExpr_ExpressedGene.py
@@ -1,22 +1,32 @@
 # - plot avg gene expression vs. expressed genes scatter plot (liam)
 # avgGeneExpr_ExpressedGene.py (liam’s)
 
+import matplotlib.pyplot as plt
 import csv
-		
-
+import numpy as np
+import os
 def main():
 
 
-	with open('treehouse_public_samples_unique_hugo_log2_tpm_plus_1.2017-09-11.tsv','rb') as tsvin, open('new.csv', 'wb') as csvout:
-	    tsvin = csv.reader(tsvin, delimiter='\t')
-	    csvout = csv.writer(csvout)
+	i = 1
 
-	    for row in tsvin:
-	        count = int(row[4])
-	        if count > 0:
-	            csvout.writerows([row[2:4] for _ in xrange(count)])
+	# sampleValues = [[],[]]
+	# with open('treehouse_public_samples_unique_hugo_log2_tpm_plus_1.2017-09-11.tsv') as tsvfile:
+	# 	reader = csv.reader(tsvfile, delimiter='\t')
+	# 	for col in reader :
+	# 		sampleValues.append(col)
+	fname = 'treehouse_public_samples_unique_hugo_log2_tpm_plus_1.2017-09-11.tsv'
 
+	x,y = np.loadtxt(fname, dtype='float', delimiter='\t', unpack=True)
 
+	fname = os.tmpnam()
+	np.fromfile('treehouse_public_samples_unique_hugo_log2_tpm_plus_1.2017-09-11.tsv', )
+
+	print(sampleValues)
+	# print(x)
+	# x = np.loadtxt('treehouse_public_samples_unique_hugo_log2_tpm_plus_1.2017-09-11.tsv', delimiter='\t', unpack=True, skiprows = 62742)
+	zeroCount = [i for i, e in enumerate(sampleValues) if e != 0]
+	plt.hist(zeroCount)
 
 if __name__ == "__main__":
 	main()  # delete the list when you want to run normally
