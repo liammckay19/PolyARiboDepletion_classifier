6885f91981b8ba9e715ad1315405d4c1681755de
Liam McKay <ltmckay@ucsc.edu>
2018-03-07 19:49:42 -0800

    use pandas for loading files

--- avgGeneExpr_ExpressedGene.py
+++ avgGeneExpr_ExpressedGene.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import csv
 import numpy as np
 import os
+import pandas as pd
 def main():
 
 
@@ -16,13 +17,16 @@ def main():
 	# 	for col in reader :
 	# 		sampleValues.append(col)
 	fname = 'treehouse_public_samples_unique_hugo_log2_tpm_plus_1.2017-09-11.tsv'
+	print(fname)
 
-	x,y = np.loadtxt(fname, dtype='float', delimiter='\t', unpack=True)
 
-	fname = os.tmpnam()
-	np.fromfile('treehouse_public_samples_unique_hugo_log2_tpm_plus_1.2017-09-11.tsv', )
+	data = pd.read_csv(fname, sep="\t", header=None)
 
-	print(sampleValues)
+
+
+	# x,y = np.loadtxt(fname, delimiter='\t', unpack=True)
+	# print(x,y)
+	# print(sampleValues)
 	# print(x)
 	# x = np.loadtxt('treehouse_public_samples_unique_hugo_log2_tpm_plus_1.2017-09-11.tsv', delimiter='\t', unpack=True, skiprows = 62742)
 	zeroCount = [i for i, e in enumerate(sampleValues) if e != 0]
