dedbbbdfa16fff78e5a17d8e3f64d3d46bf0e14a
Liam McKay <ltmckay@ucsc.edu>
2018-03-16 15:17:05 -0700

    FINAL COMMIT

--- avgGeneExpr_ExpressedGene.py
+++ avgGeneExpr_ExpressedGene.py
@@ -1,31 +1,39 @@
-# - plot avg gene expression vs. expressed genes scatter plot (liam)
-# avgGeneExpr_ExpressedGene.py (liam’s)
+# - plot avg gene expression vs. expressed genes scatter plot
+# avgGeneExpr_ExpressedGene.py 
 
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
 import pandas as pd
 
+# run python within the file containing this tsv file
 fname = 'treehouse_public_samples_unique_hugo_log2_tpm_plus_1.2017-09-11.tsv'
 print(fname)
 ckccPubSamples = pd.read_csv(fname, sep="\t")
+# loads data
+
 
 # TARGET-51
 # THR21
 # TH01
 # these are ribominus
 
-# reading data and initial calculations: 
+## reading data and initial calculations: 
 
+# find all PolyA selected Samples from reference set (TranscriptMethod_THPEDv1.csv)
 polyA = [c for c in ckccPubSamples.columns if c[:4] != 'TH01']
 df1=ckccPubSamples[polyA]
 polyA = [c for c in df1.columns if c[:5] != 'THR21']
 df2=df1[polyA]
 polyA = [c for c in df2.columns if c[:9] != 'TARGET-51']
 polyAdf=df2[polyA]
+# stores dataframe of all polyA samples to be read later
+polyAdf.to_pickle('polyAdf.pkl')
 
+# attempt to calculate mean of all polyA samples
 polyAdf['mean'] = polyAdf.mean(axis=1)
 
+# current way of getting 95th percentile, variance and mean for polyA
 polyAVarianceDf = polyAdf.var(axis=0)
 polyAMeanDf = polyAdf.mean(axis=0)
 polyAPctlDf = polyAdf.quantile(0.95)
@@ -33,11 +41,12 @@ polyAVarianceDf.to_pickle('polyAVarDf.pkl')
 polyAMeanDf.to_pickle('polyAMeanDf.pkl')
 polyAPctlDf.to_pickle('polyAPtclDf.pkl')
 
+# gather all riboD samples into a dataframe
 suspectedRiboD = ("TH01_0051_S01","TH01_0053_S01","TH01_0054_S01","TH01_0055_S01","TH01_0061_S01","TH01_0062_S01","TH01_0063_S01","TH01_0064_S01","TH01_0069_S01")
-
 riboD = [c for c in data.columns if (c[:5] == 'THR21' or c[:9] == 'TARGET-51' or c[:4] == 'TH01') and c[:13] not in suspectedRiboD]
 riboDdf=data[riboD]
 
+# current way of getting 95th percentile, variance and mean for polyA
 riboDVarianceDf = riboDdf.var(axis=0)
 riboDMeanDf = riboDdf.mean(axis=0)
 riboDPctlDf = riboDdf.quantile(0.95)
@@ -47,7 +56,6 @@ riboDPctlDf.to_pickle('riboDPtclDf.pkl')
 
 
 
-# distance calculations:
 riboDp95 = riboDPctlDf.mean(axis=0)
 riboDVar = riboDVarianceDf.mean(axis=0)
 riboDMean = riboDMeanDf.mean(axis=0)
@@ -72,17 +80,18 @@ polyAMean = polyAMeanDf.mean(axis=0)
 # 0.7170628642305512
 
 
+# read data from previously stored data
 polyAVarianceDf = pd.read_pickle('polyAVarDf.pkl')
 polyAMeanDf = pd.read_pickle('polyAMeanDf.pkl')
 polyAPctlDf = pd.read_pickle('polyAPtclDf.pkl')
-#plot data
+# plot data
 plt.boxplot((polyAPctlDf, polyAVarianceDf, polyAMeanDf))
-#labels
+# labels
 plt.xlabel("this is X")
 plt.ylabel("this is Y")
-#title
+# title
 plt.title("PolyA p95, Variance, Mean")
-#show plot
+# show plot
 plt.show()
 
 riboDVarianceDf = pd.read_pickle('riboDVarDf.pkl')
@@ -91,39 +100,10 @@ riboDPctlDf = pd.read_pickle('riboDPtclDf.pkl')
 
 # Distance calculation of mean
 threeValsRiboD = np.array([riboDp95[0],riboDVar[0],riboDMean[0]])
-# figure out how to access mean value in dataframes of one value
 
+# figure out how to access mean value in dataframes of one value
 threeValsPolyA = np.array([polyAp95[0],polyAVar[0],polyAMean[0]])
 
-
-# new sample
-# newSampVarianceDf = newSampdf.var(axis=0)
-# newSampMeanDf = newSampdf.mean(axis=0)
-# newSampPctlDf = newSampdf.quantile(0.95)
-# newSampVarianceDf.to_pickle('newSampVarDf.pkl')
-# newSampMeanDf.to_pickle('newSampMeanDf.pkl')
-# newSampPctlDf.to_pickle('newSampPtclDf.pkl')
-
-# for col in newSampPctlDf.columns :
-# 	threeValsNewSamp = np.array([newSampPctlDf[col],newSampVarianceDf[col],newSampMeanDf[col]])
-# 	differenceToRiboD = np.subtract(threeValsNewSamp, threeValsRiboD)
-# 	differenceToRiboD = np.absolute(differenceToRiboD)
-# 	differenceToPolyA = np.subtract(threeValsNewSamp, threeValsPolyA)
-# 	differenceToPolyA = np.absolute(differenceToPolyA)
-
-# 	if(differenceToPolyA > differenceToRiboD):
-# 		print("{0}\tRiboD".format(col))
-# 	elif(differenceToPolyA < differenceToRiboD):
-# 		print("{0}\tPolyA".format(col))
-
-
-# # x,y = np.loadtxt(fname, delimiter='\t', unpack=True)
-# # print(x,y)
-# # print(sampleValues)
-# # print(x)
-# # x = np.loadtxt('treehouse_public_samples_unique_hugo_log2_tpm_plus_1.2017-09-11.tsv', delimiter='\t', unpack=True, skiprows = 62742)
-# zeroCount = [i for i, e in enumerate(sampleValues) if e != 0]
-# plt.hist(zeroCount)
 
 if __name__ == "__main__":
 	main()  # delete the list when you want to run normally
--- polyAriboDclassifier-v2.py
+++ polyAriboDclassifier-v2.py
@@ -1,4 +1,16 @@
-# riboD-polyA-classifier.py
+#!/usr/bin/env python3
+# By Liam McKay (ltmckay) and Paola Angulo (pangulo)
+# polyAriboDclassifier-v2.py
+
+
+
+####################################################################################
+# MAIN PROGRAM
+# 
+# Finds the method of preparation for the transcriptome from a sequence partner 
+# in the Treehouse Initiative 
+# Usage: $ python polyAriboDclassifier-v2.py [-nG [--noGene]] inputFile.tsv outputFile
+####################################################################################
 
 
 import matplotlib.pyplot as plt
@@ -6,29 +18,71 @@ import csv
 import numpy as np
 import pandas as pd
 import sys
+########################################################################
+# CommandLine
+########################################################################
+class CommandLine() :
+	'''
+	Handle the command line, usage and help requests.
 
-def main():
+	CommandLine uses argparse, now standard in 2.7 and beyond. 
+	it implements a standard command line argument parser with various argument options,
+	a standard usage and help.
 
-	inputFileName = sys.stdin
-	newSampdf = pd.read_csv(inputFileName, sep="\t")
-	# print(newSampdf)	
+	attributes:
+	all arguments received from the commandline using .add_argument will be
+	avalable within the .args attribute of object instantiated from CommandLine.
+	For example, if myCommandLine is an object of the class, and requiredbool was
+	set as an option using add_argument, then myCommandLine.args.requiredbool will
+	name that option.
+ 
+	'''
+	
+	def __init__(self, inOpts=None) :
+		'''
+		Implement a parser to interpret the command line argv string using argparse.
+		'''
+		
+		import argparse
+		self.parser = argparse.ArgumentParser(description = 'Find the method of sequencing of a gene expression column', 
+											 epilog = 'Uses 95th pctl, variance, mean calculation of new and old reference samples', 
+											 add_help = True, #default is True 
+											 prefix_chars = '-', 
+											 usage = '%(prog)s [options] -option1[default] <input >output'
+											 )
+		self.parser.add_argument('inFile', action = 'store', help='input file name')
+		self.parser.add_argument('outFile', action = 'store', help='output file name') 
+		self.parser.add_argument('-nG', '--noGene', action = 'store', nargs='?', const=True, default=False, help='Use this flag if there is no Gene column')
+		self.parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')  
+		if inOpts is None :
+			self.args = self.parser.parse_args()
+		else :
+			self.args = self.parser.parse_args(inOpts)
 
-# # >>> polyAp95
-# 5.340720200699123
-# >>> polyAVar
-# 3.649240487654772
-# >>> polyAMean
-# 1.0491995456139986
+def main(inCL=None):
+	'''
+	Find PolyA-Selection or Ribo-Depletion of a column of gene expression data by 
+	comparing the 95th percentile, variance, and mean of a reference set to the new
+	sample.
+	Command Line arguments: 
+	-nG --noGene: use if your new sample does not have a "Gene" column, otherwise 
+				  the program assumes that there is a Gene column and removes it.
+
+	'''
+	if inCL is None:
+		myCommandLine = CommandLine()
+	else :
+		myCommandLine = CommandLine(inCL)
 
 
-# >>> riboDp95
-# 3.6812559832605625
-# >>> riboDVar
-# 1.881014066540907
-# >>> riboDMean
-# 0.7170628642305512
+	# open file for output
+	inputFile = myCommandLine.args.inFile 
+	outputFile = myCommandLine.args.outFile 
+	outputFileObject = open(outputFile, 'w')
+	newSampdf = pd.read_csv(inputFile, sep="\t")
 
-	# distance calculations:
+	# ------------------------------------------------
+	# classification calculations:
 
 	# Load these in from values returned by avgGeneExpr_ExpressedGene.py
 	riboDp95 = 3.6812559832605625
@@ -40,48 +94,39 @@ def main():
 
 	# Distance calculation of mean
 	threeValsRiboD = np.array([riboDp95,riboDVar,riboDMean])
-	# figure out how to access mean value in dataframes of one value
 
+	# figure out how to access mean value in dataframes of one value
 	threeValsPolyA = np.array([polyAp95,polyAVar,polyAMean])
 
-	
-	
 	# new sample
 	newSampVarianceDf = newSampdf.var(axis=0)
 	newSampMeanDf = newSampdf.mean(axis=0)
 	newSampPctlDf = newSampdf.quantile(0.95)
 
-	newSampdf = newSampdf.drop("Gene", axis=1)
+	# drop gene column in the new sample's dataframe
+	if(myCommandLine.args.noGene == False):
+		newSampdf = newSampdf.drop("Gene", axis=1)
+
 
 	for col in newSampdf.columns :
 		threeValsNewSamp = np.array([newSampPctlDf[col],newSampVarianceDf[col],newSampMeanDf[col]])
-		# differenceToRiboD = np.subtract(threeValsNewSamp, threeValsRiboD)
-		# differenceToRiboD = np.absolute(differenceToRiboD).sum()
-		# differenceToPolyA = np.subtract(threeValsNewSamp, threeValsPolyA)
-		# differenceToPolyA = np.absolute(differenceToPolyA).sum()
-		# print(threeValsNewSamp)
-
-		exprErrorArrayRiboD = np.divide(np.subtract(threeValsNewSamp,threeValsRiboD),threeValsRiboD)
-		# print(exprErrorArrayRiboD)
-		exprErrorArrayPolyA = np.divide(np.subtract(threeValsNewSamp,threeValsPolyA),threeValsPolyA)
-		# print(exprErrorArrayPolyA)
-
-		# print("polya", differenceToPolyA)
-		# print("ribod", differenceToRiboD)
-		averageExprErrorRiboD = abs(np.average(exprErrorArrayRiboD))
-		averageExprErrorPolyA = abs(np.average(exprErrorArrayPolyA))
+		differenceToRiboD = np.subtract(threeValsNewSamp, threeValsRiboD)
+		differenceToPolyA = np.subtract(threeValsNewSamp, threeValsPolyA)
+		
+		# scoring method based on how close each p95, var, mean is to the respective reference method
 		riboDScore = 0
 		polyAScore = 0
 		for i in range(0,3):
-			if (abs(exprErrorArrayPolyA[i])<abs(exprErrorArrayRiboD[i])):
+			if (abs(differenceToPolyA[i])<abs(differenceToRiboD[i])):
 				polyAScore+=1
 			else:
 				riboDScore+=1
-			# print(abs(exprErrorArrayPolyA[i]),abs(exprErrorArrayRiboD[i]))
-
 		if(riboDScore>polyAScore):
-			print("{0}\tRiboMinus".format(col))
+			outputFileObject.write("{0}\tRiboMinus\n".format(col))
 		else:
-			print("{0}\tPolyA".format(col))
+			outputFileObject.write("{0}\tPolyA\n".format(col))
 
-main()
+	
+if __name__ == "__main__":
+	main() 
+
